a7e480af3a60e6626550d8c605a091d583cc095a
RitikaPunjabi <69786032+RitikaPunjabi@users.noreply.github.com>
2020-08-25 20:49:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+const ENGINE=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+function setup() {
+  createCanvas(400,400);
+  engine=ENGINE.create();
+  world=engine.world;
+  object=Bodies.rectangle(200,100,50,50);
+  World.add(world.object);
+}
+
+function draw() {
+  background(0);  
+ 
+  var rect=createSprite(200, 200, 50, 50);
+
+  drawSprites();
+}
